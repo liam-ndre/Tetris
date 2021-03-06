ebd3bde23ffde230c9ecfac827b01640d80b8a12
liam-ndre <liam.eguia2@mail.dcu.ie>
2021-03-06 10:19:22 +0000

    grid creation

--- tetris.py
+++ tetris.py
@@ -185,3 +185,19 @@ def get_shape():
     global shapes, shape_colors
  
     return Piece(5, 0, random.choice(shapes))
+
+    surface.fill((0,0,0))
+    # Tetris Title
+    font = pygame.font.SysFont(\'comicsans\', 60)
+    label = font.render(\'TETRIS\', 1, (255,255,255))
+ 
+    surface.blit(label, (top_left_x + play_width / 2 - (label.get_width() / 2), 30))
+ 
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            pygame.draw.rect(surface, grid[i][j], (top_left_x + j* 30, top_left_y + i * 30, 30, 30), 0)
+ 
+    # draw grid and border
+    draw_grid(surface, 20, 10)
+    pygame.draw.rect(surface, (255, 0, 0), (top_left_x, top_left_y, play_width, play_height), 5)
+    pygame.display.update()
