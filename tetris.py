6500fa8eb8b587d01b3f277d16676564f983d0e4
liam-ndre <liam.eguia2@mail.dcu.ie>
2021-03-06 15:23:26 +0000

    adjusted shape positions

--- tetris.py
+++ tetris.py
@@ -143,3 +143,19 @@ def create_grid(locked_pos={}):  # *
     return grid
 
 
+def convert_shape_format(shape):
+    positions = []
+    format = shape.shape[shape.rotation % len(shape.shape)]
+
+    for i, line in enumerate(format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                positions.append((shape.x + j, shape.y + i))
+
+    for i, pos in enumerate(positions):
+        positions[i] = (pos[0] - 2, pos[1] - 4)
+
+    return positions
+
+
