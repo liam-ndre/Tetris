5d90597be6573d642a684fbb5722a6e32718ebc8
liam-ndre <liam.eguia2@mail.dcu.ie>
2021-03-06 10:10:55 +0000

    created the variables

--- tetris.py
+++ tetris.py
@@ -1,2 +1,14 @@
 import pygame
 import random
+
+pygame.font.init()
+
+# Global Vars
+s_width = 800
+s_height = 700
+play_width = 300
+play_height = 600
+block_size = 30
+
+top_left_x = (s_width - play_width) // 2
+top_left_y = s_height - play_height
