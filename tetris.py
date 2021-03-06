72d7ab21b0cf2aed3770e20b349b79f7536513eb
liam-ndre <liam.eguia2@mail.dcu.ie>
2021-03-06 10:17:59 +0000

    created classes for each shape

--- tetris.py
+++ tetris.py
@@ -159,3 +159,14 @@ def main_menu():
     pass
 
 main_menu()  # start game
+
+class Piece(object):
+    rows = 20  # y
+    columns = 10  # x
+ 
+    def __init__(self, column, row, shape):
+        self.x = column
+        self.y = row
+        self.shape = shape
+        self.color = shape_colors[shapes.index(shape)]
+        self.rotation = 0  # number from 0-3
