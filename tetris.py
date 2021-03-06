4fb546595cca0d993d26aa267f11c363b47c2660
liam-ndre <liam.eguia2@mail.dcu.ie>
2021-03-06 10:16:12 +0000

    assigned the following classes for the game

--- tetris.py
+++ tetris.py
@@ -118,3 +118,44 @@ T = [['.....',
 
 shapes = [S, Z, I, O, J, L, T]
 shape_colours = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
+
+class Piece(object):
+    pass
+
+def create_grid(locked_positions={}):
+    pass
+
+def convert_shape_format(shape):
+    pass
+
+def valid_space(shape, grid):
+    pass
+
+def check_lost(positions):
+    pass
+
+def get_shape():
+    pass
+
+def draw_text_middle(text, size, color, surface):  
+    pass
+   
+def draw_grid(surface, row, col):
+    pass
+
+def clear_rows(grid, locked):
+    pass
+
+def draw_next_shape(shape, surface):
+    pass
+
+def draw_window(surface):
+    pass
+
+def main():
+    pass
+
+def main_menu():
+    pass
+
+main_menu()  # start game
