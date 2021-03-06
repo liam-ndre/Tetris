8ab949275417a10e74f113ebaa5c2100a7ee859e
liam-ndre <liam.eguia2@mail.dcu.ie>
2021-03-06 15:20:53 +0000

    edited the grid

--- tetris.py
+++ tetris.py
@@ -132,3 +132,14 @@ class Piece(object):  # *
         self.rotation = 0
 
 
+def create_grid(locked_pos={}):  # *
+    grid = [[(0,0,0) for _ in range(10)] for _ in range(20)]
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (j, i) in locked_pos:
+                c = locked_pos[(j,i)]
+                grid[i][j] = c
+    return grid
+
+
